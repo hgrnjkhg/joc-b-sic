5dbb40948bedb01722cf0580770ffb0b60656cc9
hgrnjkhg <205313624+hgrnjkhg@users.noreply.github.com>
2025-03-31 13:31:24 +0200

    moviment de les fletxes
    s'indica els botons dreta, esquerra, amunt i avall

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,4 +1,11 @@
 extends CharacterBody2D
-var direcccio := Vector2.ZERO
+var direccio := Vector2.ZERO
 func _ready() -> void:
 	position = Vector2(500, 300)
+
+var speed:= 120
+
+
+func _process(delta: float) -> void:
+	direccio = Input.get_vector("moure_amunt", "moure_avall", "moure_dreta", "moure_esquerra")
+	
